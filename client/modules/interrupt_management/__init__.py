efbd7e8762104abf3dc02026e963cef80f94753c
Sergiy Zasorin <sergiyzasorin@gmail.com>
2026-01-14 10:33:05 -0500

    Release v1.6.0.43: Client version sync with browser_use support

--- client/modules/interrupt_management/__init__.py
+++ client/modules/interrupt_management/__init__.py
@@ -26,5 +26,5 @@ __all__ = [
     'DEFAULT_INTERRUPT_CONFIG'
 ]
 
-__version__ = "1.6.0.42"
+__version__ = "1.6.0.43"
 __author__ = "Nexy Team"
--- client/modules/mode_management/__init__.py
+++ client/modules/mode_management/__init__.py
@@ -23,5 +23,5 @@ __all__ = [
     'ListeningMode'
 ]
 
-__version__ = "1.6.0.42"
+__version__ = "1.6.0.43"
 __author__ = "Nexy Team"
