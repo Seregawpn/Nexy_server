359ed5dc9e164cb44b5e094fe5dc03dd77de3d28
Sergiy Zasorin <sergiyzasorin@gmail.com>
2026-01-16 12:43:59 -0500

    Release v1.6.0.46: Client version sync with TCC Rate Limiting Fix

--- client/modules/mode_management/__init__.py
+++ client/modules/mode_management/__init__.py
@@ -23,5 +23,5 @@ __all__ = [
     'ListeningMode'
 ]
 
-__version__ = "1.6.0.45"
+__version__ = "1.6.0.46"
 __author__ = "Nexy Team"
--- client/modules/tray_controller/__init__.py
+++ client/modules/tray_controller/__init__.py
@@ -16,7 +16,7 @@ __all__ = [
     'TrayConfigManager'
 ]
 
-__version__ = "1.6.0.45"
+__version__ = "1.6.0.46"
 __author__ = "Nexy Team"
 
 
