e68f79bf9bb7b836e43f14c57c61d98ec998a8eb
Sergiy Zasorin <sergiyzasorin@gmail.com>
2026-01-27 12:39:58 -0500

    Release v1.6.0.59: Permission features

--- client/modules/screenshot_capture/__init__.py
+++ client/modules/screenshot_capture/__init__.py
@@ -45,6 +45,6 @@ __all__ = [
 ]
 
 # Версия модуля
-__version__ = "1.6.0.58"
+__version__ = "1.6.0.59"
 __author__ = "Nexy Development Team"
 __description__ = "Модуль захвата скриншотов для macOS с поддержкой JPEG"
--- client/modules/tray_controller/__init__.py
+++ client/modules/tray_controller/__init__.py
@@ -16,7 +16,7 @@ __all__ = [
     'TrayConfigManager'
 ]
 
-__version__ = "1.6.0.58"
+__version__ = "1.6.0.59"
 __author__ = "Nexy Team"
 
 
