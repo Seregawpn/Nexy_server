fd77b186d52dcc9ad5531244fe7dcaef2a374814
Sergiy Zasorin <sergiyzasorin@gmail.com>
2026-01-29 10:23:28 -0500

    Release v1.6.0.60: Permission features

--- client/integration/integrations/__init__.py
+++ client/integration/integrations/__init__.py
@@ -46,5 +46,5 @@ __all__ = [
     'BrowserProgressIntegration',
 ]
 
-__version__ = "1.6.0.59"
+__version__ = "1.6.0.60"
 __author__ = "Nexy Team"
--- client/modules/tray_controller/__init__.py
+++ client/modules/tray_controller/__init__.py
@@ -16,7 +16,7 @@ __all__ = [
     'TrayConfigManager'
 ]
 
-__version__ = "1.6.0.59"
+__version__ = "1.6.0.60"
 __author__ = "Nexy Team"
 
 
